0bba5d80cbe6c8dcef78bdc3c80776ae5f30b937
Bredlin <bredlinjose@gmail.com>
2024-07-04 12:32:37 +0530

    demo

---
+++ src/test/java/trial/script/TC001.java
@@ -0,0 +1,37 @@
+package trial.script;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import trial.base.BaseTest;
+
+public class TC001 extends BaseTest {
+	
+	String url = System.getProperty("user.dir")+"\\sample_website\\login.html";
+	
+	@Test
+	public void checkingTheLinks() {
+
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		WebElement lnkRegister = driver.findElement(By.xpath("//a[contains(text(),'Register')]"));
+		lnkRegister.click();
+				
+		WebElement lnkForgetPassowrd = driver.findElement(By.xpath("//a[contains(text(),'Forgot Password')]")); // changed
+		lnkForgetPassowrd.click();
+		
+		WebElement tbEmail = driver.findElement(By.id("email"));
+		tbEmail.sendKeys("xyz@yahoo.in");
+		
+		WebElement btnResetPassword = driver.findElement(By.xpath("//button[text()='Reset Password']"));
+		btnResetPassword.click();
+		
+		String title = driver.getTitle();
+		System.out.println("Title: "+ title);
+	}
+}
